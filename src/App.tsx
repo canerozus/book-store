c7b1ca191c77dfb17013f1b359b4f2bc0ebf2f07
Batuhan Bolat <bolatbatuhan@gmail.com>
2022-09-01 15:27:39 +0300

    appbar-add

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchAppBar from './components/navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
-        
-=======
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
->>>>>>> 505542f6cf1fdae4a80c791b42fdd8065f940764
+      <SearchAppBar />
 
-      </header>
     </div>
   );
 }
